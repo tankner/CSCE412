a59502969c3ffeb3cefc4612f833e70da3967658
Thomas Ankner <tankner@tamu.edu>
2025-01-10 14:49:20 +0800

    My first website

---
+++ html/functions.js
@@ -0,0 +1,43 @@
+const x = document.getElementsByClassName("link");
+
+function button1()
+{
+    console.log("Button 1");
+    // Get the current style from localStorage
+    currentStyle = localStorage.getItem("page_stylesheet_name");
+
+    // Toggle between styles
+    if (currentStyle === "styles.css") {
+        localStorage.setItem("page_stylesheet_name", "styles2.css");
+    } else {
+        localStorage.setItem("page_stylesheet_name", "styles.css");
+    }
+
+    // Load the updated style
+    load_style();
+}
+
+function load_style()
+{
+    pages_style = localStorage.getItem("page_stylesheet_name");
+    if (pages_style === null)
+    {
+        pages_style = "styles2.css";
+    }
+    console.log(document.getElementById("stylesheet"));
+    document.getElementById("stylesheet").href = pages_style;
+}
+
+function mode1()
+{
+    localStorage.setItem("page_stylesheet_name", "styles.css");
+    load_style();
+}
+
+function mode2()
+{
+    localStorage.setItem("page_stylesheet_name", "styles2.css");
+    load_style();
+}
+
+load_style();
